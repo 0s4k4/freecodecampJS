eb88936441766bcc19616c74d7e0a8b08c24a4ab
0s4k4 <soulbosss@gmail.com>
2024-08-19 21:54:22 -0500

    me quede en la actividad 10

---
+++ introduccion a js/basico/script.js
@@ -0,0 +1,21 @@
+let character = "Hello";
+//js tiene una serie de datos primitivos, uno de ellos es el string
+//este es inmutable osea que no se puede editar, mas sin embargo este se puede
+//cambiar el valor, se puede definir con dos comillas o una comilla
+
+console.log(character);
+//en js se puede imprimir los datos, estos se puede hacen con console.log
+//solo falta escribirlo y entre parentesis poner la variable a imprimir
+console.log("hola mundo");
+//o un texto, cadena valor.
+
+//se puede reasinar el valor de la variable, solo poniendo el nombre de la variable y el valor
+
+character = "world";
+console.log(character);
+let secondCharacter;
+
+//da error por que no se ha asignado ningun elemento
+// console.log(secondCharacter);
+secondCharacter = "Test";
+console.log(secondCharacter);
